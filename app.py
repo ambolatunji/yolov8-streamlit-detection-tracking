2875c531af67ab13f09a71438d83faedd0690afd
Dare Israila Olatunji <dareartabefe@gmail.com>
2023-10-30 14:47:53 +0100

    Adding Voice output

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Python In-built packages
 from pathlib import Path
 import PIL
+import pyttsx3  # Added import for text-to-speech
 
 # External packages
 import streamlit as st
@@ -8,6 +9,55 @@ import streamlit as st
 # Local Modules
 import settings
 import helper
+
+# the voice configuration
+engine = pyttsx3.init()
+
+# Function to speak the detection results
+def speak_detection_results(boxes, engine):
+
+  if len(boxes) > 0:
+
+    # Handle different box formats 
+    if boxes.shape[1] == 3:
+      labels = boxes[:, -1]
+      confidences = boxes[:, -1] 
+
+    elif boxes.shape[1] == 7:
+      labels = boxes[:, -2]  
+      confidences = boxes[:, -3]
+
+    else:
+      print("Unsupported box format")
+      return
+
+    for i in range(len(labels)):
+      label = labels[i]
+      confidence = confidences[i]
+      text = f"Label {label} with confidence {confidence:.2f}"
+      try:
+        engine.say(text)
+        engine.runAndWait()
+      except:
+        pass
+    if voice_output_enabled:
+        speak_detection_results(boxes, engine) # Pass engine
+
+    # Display performance metrics
+    inference_df = res[0].pandas().xyxy[0]  
+    inference_time = inference_df['_infer'].iloc[0] / 1000 # in ms
+    num_objects = len(boxes)
+    st.write(f"Performance Metrics:")
+    st.write(f"Image resolution: {image.shape[0]}x{image.shape[1]}") 
+    st.write(f"Objects detected: {num_objects}")
+    st.write(f"Total processing time: {inference_time:.1f}ms")
+
+    engine.runAndWait()
+  
+  else:
+    print("No objects detected")
+
+
 
 # Setting page layout
 st.set_page_config(
@@ -22,6 +72,9 @@ st.title("Object Detection using YOLOv8")
 
 # Sidebar
 st.sidebar.header("ML Model Config")
+
+# Add a button to control voice output
+voice_output_enabled = st.sidebar.checkbox("Enable Voice Output")
 
 # Model Options
 model_type = st.sidebar.radio(
@@ -79,19 +132,19 @@ if source_radio == settings.IMAGE:
                      use_column_width=True)
         else:
             if st.sidebar.button('Detect Objects'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
+                res = model.predict(uploaded_image, conf=confidence)
                 boxes = res[0].boxes
                 res_plotted = res[0].plot()[:, :, ::-1]
                 st.image(res_plotted, caption='Detected Image',
                          use_column_width=True)
+                if voice_output_enabled:  # Check if voice output is enabled
+                    if len(boxes) > 0:
+                        speak_detection_results(boxes, engine) # Speak the results
                 try:
                     with st.expander("Detection Results"):
                         for box in boxes:
                             st.write(box.data)
                 except Exception as ex:
-                    # st.write(ex)
                     st.write("No image is uploaded yet!")
 
 elif source_radio == settings.VIDEO:
